8a557107b1174e62ebfed9489f18bdea39ab7cd7
Og'abek Marimov <kingmarimov@gmail.com>
2025-10-17 17:06:56 +0500

    Fix migration conflict: recreate CASCADE delete migration as 0019

--- menu/migrations/0019_fix_cascade_delete.py
+++ menu/migrations/0019_fix_cascade_delete.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('menu', '0014_add_promotion_discount_fields'),
+        ('menu', '0018_alter_promotion_price'),
     ]
 
     operations = [
