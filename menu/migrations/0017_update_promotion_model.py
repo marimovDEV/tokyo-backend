3585f60a6ca8ca1661b06ddceb09501dc148cd33
Og'abek Marimov <kingmarimov@gmail.com>
2025-10-17 10:06:19 +0500

    Fix migration: add default value for price field

--- menu/migrations/0017_update_promotion_model.py
+++ menu/migrations/0017_update_promotion_model.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='promotion',
             name='price',
-            field=models.DecimalField(decimal_places=2, help_text='Aksiya narxi', max_digits=10, validators=[MinValueValidator(0)]),
+            field=models.DecimalField(decimal_places=2, default=0, help_text='Aksiya narxi', max_digits=10, validators=[MinValueValidator(0)]),
         ),
         migrations.AddField(
             model_name='promotion',
