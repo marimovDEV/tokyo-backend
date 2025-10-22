15afad471fb686c15e05cd4c1cca656dbf90a0c3
Og'abek Marimov <kingmarimov@gmail.com>
2025-10-22 11:26:52 +0500

    Fix timezone import in Promotion model
    - Added missing timezone import to fix is_expired property
    - Resolves 500 error in /api/promotions/ endpoint
    - All promotion endpoints now working correctly

--- menu/models.py
+++ menu/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
